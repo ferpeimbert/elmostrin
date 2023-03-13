921b4d63977509f318ae4e3ee9930f54e447394c
ferpeimbert <fpeimbert@LAPTOP22-FER.atci.home>
2023-03-12 22:13:43 -0700

    123Calabaza

---
+++ Maven20221227borrable/Maven20221227simple/src/main/java/org/peimbert/alpha/Maven20221227simple/App.java
@@ -0,0 +1,13 @@
+package org.peimbert.alpha.Maven20221227simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
