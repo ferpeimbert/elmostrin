11918ed98a6912723bc99cb71ed1bf6d20d50369
ferpeimbert <fpeimbert@LAPTOP22-FER.atci.home>
2023-03-12 23:41:10 -0700

    Continuando

---
+++ 20230312MavenApacheHello/20230312MavenApacheHello/src/main/java/org/peimbert/MiLogger.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.peimbert;
+
+/**
+ * @author fpeimbert
+ *
+ */
+public class MiLogger {
+
+	/**
+	 * 
+	 */
+	public MiLogger() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 20230312MavenApacheHello/20230312MavenApacheHello/src/main/java/org/peimbert/SysInfo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.peimbert;
+
+/**
+ * @author fpeimbert
+ *
+ */
+public class SysInfo {
+
+	/**
+	 * 
+	 */
+	public SysInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("directorios por defecto");
+		System.out.println("Ambiente grafico;");
+	}
+
+}
