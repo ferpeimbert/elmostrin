0827df81316504e8b4613487ab462c13e3c16611
ferpeimbert <fpeimbert@LAPTOP22-FER.atci.home>
2023-03-14 23:13:15 -0700

    Logger1

--- 20230312MavenApacheHello/20230312MavenApacheHello/src/main/java/org/peimbert/MiLogger.java
+++ 20230312MavenApacheHello/20230312MavenApacheHello/src/main/java/org/peimbert/MiLogger.java
@@ -3,11 +3,27 @@
  */
 package org.peimbert;
 
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author fpeimbert
  *
+SEVERE (highest)
+WARNING
+INFO
+CONFIG
+FINE
+FINER
+FINEST
  */
+
+
 public class MiLogger {
+	private final static Logger L = 
+//			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	/**
 	 * 
@@ -21,8 +37,17 @@ public class MiLogger {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// Solo comentario
-		System.out.println("todo");
+		L.setLevel(Level.INFO);
+		System.out.println();
+		System.out.println();
+		System.out.println(L.getLevel());
+		System.out.println(L.getName());
+		System.out.println(L.getParent());
+		System.out.println(L.getGlobal());
+		
+
 	}
+
+
 
 }
---
+++ 20230312MavenApacheHello/20230312MavenApacheHello/src/main/java/org/peimbert/MyHandler.java
@@ -0,0 +1,27 @@
+package org.peimbert;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.StreamHandler;
+
+public class MyHandler extends StreamHandler {
+
+    @Override
+    public void publish(LogRecord record) {
+        //add own logic to publish
+        super.publish(record);
+    }
+
+
+    @Override
+    public void flush() {
+        super.flush();
+    }
+
+
+    @Override
+    public void close() throws SecurityException {
+        super.close();
+    }
+
+}
